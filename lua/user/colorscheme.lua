9eed7f9806ce6ad81ed974c5d60d0993fdd11d85
Brian Russell <wsl@home.com>
2021-12-21 18:18:00 +0000

    added dracula

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,7 +1,8 @@
-local colorscheme = "aurora"
+local colorscheme = "dracula"
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 
 if not status_ok then
     vim.notify("colorscheme " .. colorscheme .. " not found!")
     return
 end
+vim.g.dracula_lualine_bg_color = "#44475a"
--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -47,6 +47,8 @@ return packer.startup(function(use)
   use "nvim-lua/popup.nvim"     -- An implementation of the Popup API from vim in Neovim
   use "nvim-lua/plenary.nvim"   -- Useful lua functions used by lots of plugins
   use "lunarvim/colorschemes"
+  use "Mofiqul/dracula.nvim"    -- Dracula Colour scheme
+  use "nvim-lualine/lualine.nvim"
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
@@ -54,3 +56,5 @@ return packer.startup(function(use)
     require("packer").sync()
   end
 end)
+
+
