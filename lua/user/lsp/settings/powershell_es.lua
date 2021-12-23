012ff91c7b8df80d740cd7e3a9788d14d8cf31b1
Brian Russell <wsl@home.com>
2021-12-23 00:03:53 +0000

    fixed powershell settings

--- lua/user/lsp/settings/powershell_es.lua
+++ lua/user/lsp/settings/powershell_es.lua
@@ -1,4 +1,6 @@
 return {
-    settings = {
-    }
+    -- Unzipped https://github.com/PowerShell/PowerShellEditorServices/releases
+    -- needs to go in nvim data directory
+    -- echo stdpath("data") to find it on Windows etc
+    bundle_path = vim.fn.stdpath 'data' .. '/PowerShellEditorServices'
 }
